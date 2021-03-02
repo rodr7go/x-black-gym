0b09bbc33e1ec5884dee13587b0147665e1a8b2f
Rodrigo <rodr7go.lg@gmail.com>
2021-03-02 00:29:21 -0600

    x

---
+++ database/migrations/2021_03_02_054520_create_costs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('costs', function (Blueprint $table) {
+            $table->id();
+
+            $table->decimal('amount');
+            $table->string('name');
+            $table->boolean('offer')->default(false)->nullable();
+            $table->integer('duration_months'); // manage value in months
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('costs');
+    }
+}
