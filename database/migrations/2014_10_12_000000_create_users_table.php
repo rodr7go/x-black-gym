1ac7fae4c978bf32ded664c2d03bf9d7b5cd2316
Rodrigo <rodr7go.lg@gmail.com>
2021-03-02 00:29:06 -0600

    x

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('phone')->nullable();
+            $table->boolean('is_admin')->default(false);
+            $table->date('register_at');
+
             $table->rememberToken();
             $table->timestamps();
         });
