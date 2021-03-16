8ff1c1717a2d0feece95c86b400fc248d5d6a1df
Rodrigo <rodr7go.lg@gmail.com>
2021-03-15 21:54:50 -0600

    showing users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,24 +2,17 @@
 
 @section('content')
     @component('components.basic-card')
-        @slot('content')
-
-            @include('users.partials.table')
-
-        @endslot
-    @endcomponent
-{{--    @component('components.panel')--}}
-{{--        @slot('title')--}}
-{{--            <h2>Usuarios</h2>--}}
+        @slot('title')
+            <h2>Usuarios</h2>
 
 {{--            @can('create_users')--}}
 {{--                <a href="{{ route('users.create') }}" class="btn btn-success btn-sm pull-right">Agregar</a>--}}
 {{--            @endcan--}}
-{{--        @endslot--}}
+        @endslot
 
-{{--        @slot('content')--}}
-{{--            @include('tenant.users.partials.table')--}}
-{{--        @endslot--}}
-{{--    @endcomponent--}}
+        @slot('content')
+            @include('users.partials.table')
+        @endslot
+    @endcomponent
 
 @endsection
--- resources/views/users/partials/table.blade.php
+++ resources/views/users/partials/table.blade.php
@@ -7,58 +7,30 @@
             <th>Nombre</th>
             <th>Email</th>
             <th>Telefono</th>
-            <th>Imagen</th>
-
-            <th>Role</th>
             <th>Acciones</th>
         </tr>
     @endslot
 
     @slot('tbody')
-{{--        @foreach($users as $user)--}}
-{{--            <tr>--}}
-{{--                <td>{{ $user->id }}</td>--}}
-{{--                <td>{{ $user->name }}</td>--}}
-{{--                <td>{{ $user->email }}</td>--}}
-{{--                <td>{{ $user->phone }}</td>--}}
-{{--                <td>--}}
-{{--                    <img src="{{ $user->image }}" alt="" height="35">--}}
-{{--                </td>--}}
-{{--                <td>--}}
-{{--                    --}}{{--<ul>--}}
-{{--                    --}}{{--    @foreach($user->getRoleNames() as $roleName)--}}
-{{--                    --}}{{--        <li>{{ trans("permissions.roles.$roleName") }}</li>--}}
-{{--                    --}}{{--    @endforeach--}}
-{{--                    --}}{{--</ul>--}}
-{{--                    --}}
-{{--                    {{ $user->roles->first()->trans_es }}--}}
-{{--                </td>--}}
-{{--                <td>--}}
-{{--                    @can('edit_users')--}}
-{{--                        @if (--}}
-{{--                            ! $user->hasRole(['super-admin', 'company-owner'])--}}
-{{--                            && $user->id != auth()->user()->id--}}
-{{--                        )--}}
-{{--                            <a href="{{ route('users.edit', [$user]) }}" class="btn btn-default btn-small text-primary" title="Editar">--}}
-{{--                                <i class="fa fa-pencil"></i>--}}
-{{--                            </a>--}}
-{{--                        @endif--}}
-{{--                    @endcan--}}
+        @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->phone }}</td>
 
-{{--                    @can('delete_users')--}}
-{{--                        @if (--}}
-{{--                            ! $user->hasRole(['super-admin', 'company-owner'])--}}
-{{--                            && $user->id != auth()->user()->id--}}
-{{--                        )--}}
-{{--                            <a href="#" class="btn btn-default btn-small text-danger swal-delete" data-form="#delete-form-{{$user->id}}" title="Eliminar">--}}
-{{--                                <i class="fa fa-trash"></i>--}}
-{{--                            </a>--}}
-{{--                            {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'DELETE', 'id' => "delete-form-{$user->id}", 'style' => 'display:none']) !!}--}}
-{{--                            {!! Form::close() !!}--}}
-{{--                        @endif--}}
-{{--                    @endcan--}}
-{{--                </td>--}}
-{{--            </tr>--}}
-{{--        @endforeach--}}
+                <td>
+                    <a href="{{ route('users.edit', [$user]) }}" class="btn btn-default btn-small text-primary" title="Editar">
+                        <i class="fa fa-pencil-alt"></i>
+                    </a>
+
+                    <a href="#" class="btn btn-default btn-small text-danger swal-delete" data-form="#delete-form-{{$user->id}}" title="Eliminar">
+                        <i class="fa fa-trash"></i>
+                    </a>
+                    {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'DELETE', 'id' => "delete-form-{$user->id}", 'style' => 'display:none']) !!}
+                    {!! Form::close() !!}
+                </td>
+            </tr>
+        @endforeach
     @endslot
 @endcomponent
